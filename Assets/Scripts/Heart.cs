09cafa15008a6e4c76fe9f256f7d4a6bd7a8d133
jasperburtmason <jasperburtmason@gmail.com>
2017-01-21 12:16:00 +1300

    working on heartbeat

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -4,14 +4,40 @@ using UnityEngine;
 
 public class Heart : MonoBehaviour
 {
+    [HideInInspector]
+    public float heartRate;
     public float heartRateMin;
     public float heartRateMax;
 
-    public float heartRate { get { return _heartRate; } }
-    private float _heartRate;
+    public float heartRateGain;
+    public float heartRateLoss;
+
+    public float sizeMin;
+    public float sizeMax;
+    public AnimationCurve sizeCurve;
 	
+    void Start()
+    {
+        heartRate = (heartRateMax - heartRateMin) / 2;
+    }
+
 	void Update()
     {
+        if(Input.GetButtonDown("Tap")) //rename?
+        {
+            heartRate += heartRateGain;
+        }
+        //maybe only when they're not tapping?
+        heartRate = Mathf.Clamp(heartRate - (heartRateLoss * Time.deltaTime), heartRateMin, heartRateMax);
+
+        if(heartRate <= heartRateMin || heartRate >= heartRateMax)
+        {
+            //lose
+        }
+        transform.localScale = new Vector3(sizeMin, sizeMin) + new Vector3(sizeMax - sizeMin, sizeMax - sizeMin) * sizeCurve.Evaluate((heartRate - heartRateMin) / heartRateMax);
+
+        Debug.Log(heartRate);
+
 		//move around
 	}
 }
