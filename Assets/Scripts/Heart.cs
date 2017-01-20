03c6ef8f35d221323ed24aadfbaa02d5035eafd7
jasperburtmason <jasperburtmason@gmail.com>
2017-01-21 11:52:51 +1300

    Adding heart script

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public float heartRateMin;
+    public float heartRateMax;
+
+    public float heartRate { get { return _heartRate; } }
+    private float _heartRate;
+	
+	void Update()
+    {
+		//move around
+	}
+}
