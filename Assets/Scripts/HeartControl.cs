05305a7cc659c9664c69bb29940daf49c926c743
tim-hub <tim_w@outlook.com>
2017-01-21 10:57:21 +1300

    test static wave add heart control and gamemanager

---
+++ Assets/Scripts/HeartControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HeartControl : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	/*
+	 * This is for control the heart by mouse or by touch screnn
+	 * 
+	 * 
+	 */
+
+	public static GameObject DraggedInstance;
+
+	Vector3 _startPosition;
+	Vector3 _offsetToMouse;
+	float _zDistanceToCamera;
+
+	#region Interface Implementations
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		DraggedInstance = gameObject;
+		_startPosition = transform.position;
+		_zDistanceToCamera = Mathf.Abs (_startPosition.z - Camera.main.transform.position.z);
+
+		_offsetToMouse = _startPosition - Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
+		);
+	}
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		if(Input.touchCount > 1)
+			return;
+		float y = transform.position.y;
+		transform.position = Camera.main.ScreenToWorldPoint (
+			new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _zDistanceToCamera)
+		) + _offsetToMouse;
+
+		//to keep the object can be only draged horizontally
+		transform.position = new Vector3 (transform.position.x, y, transform.position.z);
+	}
+
+	public void OnEndDrag (PointerEventData eventData)
+	{
+		DraggedInstance = null;
+		_offsetToMouse = Vector3.zero;
+	}
+
+	#endregion
+
+}
