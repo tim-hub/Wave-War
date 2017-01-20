48bf804f383ba7aec03294f6f7d0c9626bbad31d
jasperburtmason <jasperburtmason@gmail.com>
2017-01-20 23:12:00 +1300

    junk

---
+++ Assets/Scripts/FullscreenTexture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenTexture : MonoBehaviour
+{
+    public Material material;
+    public Texture texture;
+
+    void OnEnable() { }
+    
+    void OnPostRender()
+    {
+        material.mainTexture = texture;
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        GL.Begin(GL.QUADS);
+        for(int i = 0; i < material.passCount; i++)
+        {
+            material.SetPass(i);
+            GL.TexCoord2(0, 0);
+            GL.Vertex3(0, 0, 0);
+            GL.TexCoord2(0, 1);
+            GL.Vertex3(0, 1, 0);
+            GL.TexCoord2(1, 1);
+            GL.Vertex3(1, 1, 0);
+            GL.TexCoord2(1, 0);
+            GL.Vertex3(1, 0, 0);
+  
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    public WaveObject[] waveObjects;
+    public int points = 32;
+
+	void Update ()
+    {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.numPositions = points;
+        for(int p = 0; p < points; p++)
+        {
+            float x = (float)p / points;
+            float y = 0;
+            for(int wo = 0; wo < waveObjects.Length; wo++)
+            {
+                WaveObject waveObject = waveObjects[wo];
+                float xx = (Time.time + x) * 360 * Mathf.Deg2Rad * waveObject.frequency;
+                switch (waveObject.function)
+                {
+                    case WaveObject.Function.Sine:
+                        y += Mathf.Sin(xx) * waveObject.amplitude;
+                        break;
+
+                    case WaveObject.Function.Square:
+                        y += Mathf.Sign(Mathf.Sin(xx)) * waveObject.amplitude;
+                        break;
+
+                    case WaveObject.Function.Triangle:
+                        y += Mathf.Abs((xx++ % 2) - 1);
+                        break;
+                }
+            }
+                
+            lineRenderer.SetPosition(p, new Vector2(x - 0.5f, y));
+        }
+	}
+}
