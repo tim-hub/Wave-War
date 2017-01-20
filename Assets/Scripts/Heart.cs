c85a72ef64a3e3040a0b0839662f8029219d911d
tim-hub <tim_w@outlook.com>
2017-01-21 12:14:10 +1300

    add a hear movement control

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -7,11 +7,27 @@ public class Heart : MonoBehaviour
     public float heartRateMin;
     public float heartRateMax;
 
+	public float heartMovingSpeed=1f;
+
+
     public float heartRate { get { return _heartRate; } }
     private float _heartRate;
-	
+
+	private Rigidbody2D rgb2d;
+
+
+	void Start(){
+		rgb2d = GetComponent <Rigidbody2D>();
+
+	}
+
 	void Update()
     {
 		//move around
+		Vector2 movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"))*heartMovingSpeed *Time.deltaTime;
+
+		rgb2d.MovePosition (rgb2d.position + movement);
+
+
 	}
 }
