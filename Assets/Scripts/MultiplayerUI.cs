0473f6d0a4dbf13e295ef29c0791e1fbcae6714b
jasperburtmason <jasperburtmason@gmail.com>
2017-01-21 15:42:27 +1300

    more networking

---
+++ Assets/Scripts/MultiplayerUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MultiplayerUI : MonoBehaviour
+{
+    public MultiplayerManager multiplayerManager;
+    public InputField ipField;
+    public InputField portField;
+
+    public void CreateServer()
+    {
+        multiplayerManager.CreateServer(int.Parse(portField.text));
+    }
+
+    public void JoinServer()
+    {
+
+    }
+}
