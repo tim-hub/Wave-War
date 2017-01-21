41a501fe1be7688de7f8117719921d7423d5619c
jasperburtmason <jasperburtmason@gmail.com>
2017-01-21 14:05:07 +1300

    lots of junk

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -11,21 +11,25 @@ public class Heart : MonoBehaviour
 
 	public float heartMovingSpeed=1f;
 
-
     public float heartRateGain;
     public float heartRateLoss;
 
     public float sizeMin;
     public float sizeMax;
     public AnimationCurve sizeCurve;
+    public float rotationSpeed;
     public float rotationAmount;
+    public float rotationResetSpeed;
     public AnimationCurve rotationCurve;
     public Gradient colourGradient;
-	
-    
-	  private float _heartRate;
 
-	  private Rigidbody2D rgb2d;
+    public Wave[] waves;
+    public float frequencyMin;
+    public float frequencyMax;
+    public float frequencySpeed;
+
+	
+	private Rigidbody2D rgb2d;
     
     void Start()
     {
@@ -48,12 +52,19 @@ public class Heart : MonoBehaviour
         }
 
         float t = (heartRate - heartRateMin) / heartRateMax;
-        transform.localScale = new Vector3(sizeMin, sizeMin) + new Vector3(sizeMax - sizeMin, sizeMax - sizeMin) * sizeCurve.Evaluate(t);
-        //transform.Rotate(new Vector3(0, 0, 1), Mathf.PingPong(Time.time, rotation)
-        GetComponent<SpriteRenderer>().color = colourGradient.Evaluate(t);
-		//move around
-		Vector2 movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"))*heartMovingSpeed *Time.deltaTime;
 
+        foreach (Wave wave in waves)
+        {
+            wave.scale = Mathf.Lerp(wave.scale, frequencyMin + (frequencyMax - frequencyMin) * t, Time.deltaTime * frequencySpeed);
+        }
+
+
+        transform.localScale = new Vector3(sizeMin, sizeMin) + new Vector3(sizeMax - sizeMin, sizeMax - sizeMin) * sizeCurve.Evaluate(t);
+        transform.Rotate(new Vector3(0, 0, 1), (Mathf.PingPong(Time.time * rotationSpeed, 1f) - 0.5f) * rotationAmount * rotationCurve.Evaluate(t) * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.identity, Time.deltaTime * rotationResetSpeed * (1f - rotationCurve.Evaluate(t)));
+        GetComponent<SpriteRenderer>().color = colourGradient.Evaluate(t);
+
+		Vector2 movement = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"))*heartMovingSpeed *Time.deltaTime;
 		rgb2d.MovePosition (rgb2d.position + movement);
 	}
 }
