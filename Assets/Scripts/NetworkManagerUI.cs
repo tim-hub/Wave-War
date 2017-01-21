a7e3b809416c02556ff4553fabe9b9b088130ffb
jasperburtmason <jasperburtmason@gmail.com>
2017-01-21 14:40:12 +1300

    working on server

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    public NetworkManager networkManager;
+    public InputField matchNameField;
+
+    public void StartHost()
+    {
+        networkManager.matchName = matchNameField.text;
+        networkManager.StartHost();
+    }
+}
